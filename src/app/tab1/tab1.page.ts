43c85cfe69f2896d7459f3d5f1e437f415e2a8e3
unknown <elaine.ra96@gmail.com>
2022-10-06 08:28:56 -0300

    Tab1 Finalizado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,91 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  valor1: number;
+  valor2: number;
+  resultado: any;
+  operacao: string;
+
+  constructor( private alertController: AlertController ) {}
+
+  verificarValor(){
+    return this.valor1 && this.valor2;
+  }
+  soma(){
+    if (this.verificarValor()){
+    this.resultado = this.valor1 + this.valor2;
+    this.operacao = `${this.valor1} + ${this.valor2}`;
+  }else{
+    this.presentAlert('somar');
+  }
+  }
+
+  div(){
+    if (this.verificarValor()){
+    this.resultado = this.valor1 / this.valor2;
+    this.operacao = `${this.valor1} / ${this.valor2}`;
+  }else{
+    this.presentAlert('dividir');
+  }
+  }
+
+  mult(){
+    if (this.verificarValor()){
+    this.resultado = this.valor1 * this.valor2;
+    this.operacao = `${this.valor1} * ${this.valor2}`;
+  }else{
+    this.presentAlert('multiplicar');
+  }
+  }
+
+  subt(){
+    if (this.verificarValor()){
+    this.resultado = this.valor1 - this.valor2;
+    this.operacao = `${this.valor1} - ${this.valor2}`;
+  }else{
+    this.presentAlert('subtrair');
+  }
+  }
+
+  menor(){
+    if (this.verificarValor()){
+      if(this.valor1 < this.valor2){
+        this.resultado = `Verdadeiro`;
+        this.operacao = `${this.valor1} < ${this.valor2}`;
+      }else{
+        this.resultado = `Falso`;
+        this.operacao = `${this.valor1} < ${this.valor2}`;
+      }
+     }
+    else{
+      this.presentAlert('verificar');
+    }
+  }
+
+  maior(){
+    if (this.verificarValor()){
+      if(this.valor1 > this.valor2){
+        this.resultado = `Verdadeiro`;
+        this.operacao = `${this.valor1} > ${this.valor2}`;
+      }else{
+        this.resultado = `Falso`;
+        this.operacao = `${this.valor1} > ${this.valor2}`;
+      }
+     }
+    else{
+      this.presentAlert('verificar');
+    }
+  }
+
+  async presentAlert(operacao) {
+    const alert = await this.alertController.create({
+      header: 'ERRO',
+      subHeader: `impossivel ${operacao}`,
+      message: `um ou mais valores nulos`,
+      buttons: ['entendi, não serei burro'],
+    });
+
+    await alert.present();
+  }
 
 }
